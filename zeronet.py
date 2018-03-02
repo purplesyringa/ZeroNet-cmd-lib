70c0b4eeb9f0dfec6f62cf9a44783217856b5f28
Ivanq <imachug@yandex.ru>
2018-03-02 20:13:06 +0300

    Add 'config set'

--- zeronet.py
+++ zeronet.py
@@ -8,8 +8,8 @@ from lib.config import config
 class ZeroNet(Callable.WithHelp):
 	"""
 		Commands:
-		help                    Print this help
-		config                  Get or set config values
+		help                        Print this help
+		config                      Get or set config values
 
 		Use 'help <command>' or 'help <command> <subcommand>' for more info
 	"""
@@ -19,7 +19,8 @@ class ZeroNet(Callable.WithHelp):
 			Get or set config values
 
 			Subcommands:
-			config list             Print list of all saved values as newline-separated values
+			config list                 Print list of all saved values as newline-separated values
+			config set                  Set config value
 		"""
 
 		raise Callable.SubCommand
@@ -29,10 +30,20 @@ class ZeroNet(Callable.WithHelp):
 			Print list of all saved values as newline-separated values
 
 			Usage:
-			config list             Print all values
-			config list <prefix>    Print all values beginning with <prefix>
+			config list                 Print all values
+			config list <prefix>        Print all values beginning with <prefix>
 		"""
 
 		print "\n".join(filter(lambda name: name.startswith(prefix), config.list()))
+
+	def actionConfigSet(self, name, value):
+		"""
+			Set config value
+
+			Usage:
+			config set <name> <value>   Set config variable <name> to <value>. <name> can be dot-separated.
+		"""
+
+		config.set(name, value)
 
 ZeroNet(argv[0] if argv != [] else "help", argv[1:])
