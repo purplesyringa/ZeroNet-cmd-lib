56379129b3eff791076cc07335652e7c368e3c95
Ivanq <imachug@yandex.ru>
2018-03-02 19:50:13 +0300

    Add 'config'

--- zeronet.py
+++ zeronet.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python
 
-import sys, inspect
+import sys
 from lib.callable import Callable
 from lib.args import argv
+from lib.config import config
 
 class ZeroNet(Callable.WithHelp):
 	"""
 		Commands:
-		help            Print this help
+		help                    Print this help
+		config                  Get or set config values
+
+		Use 'help <command>' or 'help <command> <subcommand>' for more info
 	"""
+
+	def actionHelp(self, cmd="", *sub):
+		if cmd == "help":
+			self.actionHelp("", *sub)
+		elif cmd == "config":
+			print "Print list of all saved values as newline-separated values"
+			print ""
+			print "Usage:"
+			print "config                  Print all values"
+			print "config <prefix>         Print all values beginning with <prefix>"
+		else:
+			super(ZeroNet, self).actionHelp(cmd, *sub)
+
+	def actionConfig(self, prefix=None):
+		print "\n".join(filter(lambda name: name.startswith(prefix), config.list()))
 
 ZeroNet(argv[0] if argv != [] else "help", argv[1:])
