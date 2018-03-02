b3818c032e0e15bce3b1c83b7e7f8f8a469f8bb3
Ivanq <imachug@yandex.ru>
2018-03-02 17:35:44 +0300

    Check that call is secure

--- zeronet.py
+++ zeronet.py
@@ -2,6 +2,7 @@
 
 import sys
 from lib.args import argv
+from lib.config import config
 
 class ZeroNet(object):
 	def __init__(self, cmd, args):
@@ -12,7 +13,39 @@ class ZeroNet(object):
 			sys.stderr.write("Unknown command %s. Allowed commands are: %s\n" % (cmd, ", ".join(all_commands)))
 			return
 
-		handler()
+		if self.checkCall(cmd, handler, args):
+			handler(*args)
+
+	def checkCall(self, cmd, func, args):
+		import inspect
+
+		expected_args = inspect.getargspec(func).args[1:] # Split "self"
+		varargs = inspect.getargspec(func).varargs
+		defaults = inspect.getargspec(func).defaults or tuple()
+
+		if varargs is not None:
+			if len(args) >= len(expected_args) - len(defaults):
+				return True
+		else:
+			if len(expected_args) - len(defaults) <= len(args) <= len(expected_args):
+				return True
+
+		if varargs is not None:
+			sys.stderr.write("%s expected %s argument(s) or more, %s given\n" % (cmd, len(expected_args), len(args)))
+			expected_args += ["*%s" % varargs]
+		elif len(defaults) > 0:
+			sys.stderr.write("%s expected from %s to %s argument(s), %s given\n" % (cmd, len(expected_args) - len(defaults), len(expected_args), len(args)))
+		else:
+			sys.stderr.write("%s expected %s argument(s), %s given\n" % (cmd, len(expected_args), len(args)))
+
+		if len(defaults) > 0:
+			default_args = reversed(zip(reversed(expected_args), reversed(defaults)))
+			default_args = map(lambda arg: "%s=%s" % arg, default_args)
+			expected_args = expected_args[:-len(default_args)] + default_args
+
+		sys.stderr.write("Arguments: %s\n" % ", ".join(expected_args))
+
+		return False
 
 	def actionHelp(self):
 		print "Commands:"
