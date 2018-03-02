74327274b742a59ff8a5af8c0175e32891d9bc7c
Ivanq <imachug@yandex.ru>
2018-03-02 19:49:04 +0300

    Use Callable.WithHelp

--- zeronet.py
+++ zeronet.py
@@ -4,16 +4,10 @@ import sys, inspect
 from lib.callable import Callable
 from lib.args import argv
 
-class ZeroNet(Callable):
+class ZeroNet(Callable.WithHelp):
 	"""
 		Commands:
 		help            Print this help
 	"""
-
-	def actionHelp(self, cmd="help"):
-		if cmd == "help":
-			print inspect.cleandoc(ZeroNet.__doc__)
-		else:
-			print "Unknown topic '%s'" % cmd
 
 ZeroNet(argv[0] if argv != [] else "help", argv[1:])
