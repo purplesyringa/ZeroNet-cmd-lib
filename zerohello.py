5901dddb2a666493ef3c980c89eb160341419fcd
Ivanq <imachug@yandex.ru>
2018-03-03 20:00:37 +0300

    Add 'zerohello site pause'

--- zerohello.py
+++ zerohello.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 
-import sys, os, signal as os_signal
-import sqlite3, json
+import sys
 from lib.callable import Callable
 from lib.args import argv
 from lib.config import config
 import zeronet_lib.site as Site
 import zeronet_lib.addresses as Addresses
+from zeronet_lib.zerowebsocket import ZeroWebSocket
 
 class ZeroHello(Callable.WithHelp):
 	"""
 		Commands:
 		help                        Print this help
+		site                        Edit or get some info about site
 
 		Use 'help <command>' or 'help <command> <subcommand>' for more info
 	"""
@@ -28,10 +29,44 @@ class ZeroHello(Callable.WithHelp):
 		else:
 			raise Callable.SubCommand
 
+	def actionSite(self, *args, **kwargs):
+		"""
+			Edit or get some info about site
+
+			Subcommands:
+			site pause                  Pause site
+		"""
+
+		raise Callable.SubCommand
+
+	def actionSitePause(self, address):
+		"""
+			Pause site
+
+			Usage:
+			site pause <address>        Stop seeding <address>
+		"""
+
+		try:
+			with self.connect(self.getAddress()) as ws:
+				ws.send("sitePause", address=address)
+		except ZeroWebSocket.Error as e:
+			sys.stderr.write("%s\n" % "\n".join(e))
+			return 1
+
 	def getDataDirectory(self):
 		return config.get("data_directory", "%s/data" % config["root_directory"])
 	def getAddress(self):
 		return config.get("homepage", Addresses.ZeroHello)
+
+	def connect(self, site):
+		wrapper_key = Site.getWrapperkey(self.getDataDirectory(), site)
+
+		address = config.get("server.address", "127.0.0.1")
+		port = config.get("server.port", "43110")
+		secure = config.get("server.secure", False)
+
+		return ZeroWebSocket(wrapper_key, "%s:%s" % (address, port), secure)
 
 try:
 	sys.exit(ZeroHello(argv))
