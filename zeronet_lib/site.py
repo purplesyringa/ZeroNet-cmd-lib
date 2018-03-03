a3960e343bea3d150ccc9fc6ac4efa9cdd445e0c
Ivanq <imachug@yandex.ru>
2018-03-03 18:03:33 +0300

    Add findByDomain()

--- zeronet_lib/site.py
+++ zeronet_lib/site.py
@@ -22,3 +22,12 @@ def sqlQuery(path, query):
 	conn = sqlite3.connect(path)
 	cursor = conn.cursor()
 	return cursor.execute(query)
+
+
+def findByDomain(path, domain):
+	with open(path, "r") as f:
+		names = json.loads(f.read())
+		if domain in names:
+			return names[domain]
+		else:
+			raise KeyError("No domain %s" % domain)
