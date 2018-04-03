c8efeff6cbb9f3ef04d2868a0af72e460cac9750
Ivanq <imachug@yandex.ru>
2018-04-03 14:47:41 +0300

    Add toColor() slot

--- zeronet_lib/zapp.py
+++ zeronet_lib/zapp.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 from clirender.layout.libs.library import Library, Slot
 
 class ZeroFrame(object):
@@ -24,5 +26,18 @@ def createLib(site, ws):
 			if (fn, args) not in cached:
 				cached[fn, args] = fn(*args)
 			return cached[fn, args]
+
+		@Slot("toColor")
+		def toColor(text, saturation=30, lightness=50):
+			import colorsys
+
+			hashed = 0
+			for pos, char in enumerate(list(text)):
+				hashed += ord(char) * pos
+				hashed %= 1777
+
+			rgb = colorsys.hsv_to_rgb(hashed % 360 / 360, saturation / 100, lightness / 100)
+			r, g, b = map(lambda part: hex(int(part * 255))[2:].zfill(2), rgb)
+			return "#%s%s%s" % (r, g, b)
 
 	return ZeroFrameLib
